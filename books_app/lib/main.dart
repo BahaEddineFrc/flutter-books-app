140a55a9086abfed1d3407c30b2227b5c392825a
Baha eddine Ferchichi <baha.ferchichi@aiesec.net>
2021-03-10 14:09:00 +0100

    add a floatingActionButton to switch between images (shouldn't work)

--- books_app/lib/main.dart
+++ books_app/lib/main.dart
@@ -21,19 +21,28 @@ class MyApp extends StatelessWidget {
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var image = Image.asset('images/nyt_placeholder.png',width: 200,height: 200);
-    var image2 = Image.asset('images/nyt_title.jpg',width: 200,height: 200);
-
+    var image = Image.asset('images/nyt_placeholder.png', width: 200, height: 200);
+    var image2 = Image.asset('images/nyt_title.jpg', width: 200, height: 200);
+    var displayedImage = image;
     return Scaffold(
       appBar: AppBar(title: Text('First screen ever')),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.edit_outlined),
+          onPressed: () {
+            if (displayedImage == image) {
+              displayedImage = image2;
+            } else {
+              displayedImage = image;
+            }
+            print('changed');
+          }),
       body: Container(
         color: Colors.grey,
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              image,
-              image2,
+              displayedImage,
             ],
           ),
         ),
