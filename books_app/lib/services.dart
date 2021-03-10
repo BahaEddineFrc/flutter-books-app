2ea090fca60888a467c61e41fc7b6dc18ce99eab
Baha eddine Ferchichi <baha.ferchichi@aiesec.net>
2021-03-10 15:24:19 +0100

    add getBooks service

--- books_app/lib/services.dart
+++ books_app/lib/services.dart
@@ -1,12 +1,25 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-  Future<String> fetchJoke() async {
-    var response = await http.get("https://api.chucknorris.io/jokes/random?category=dev");
-    if (response.statusCode == 200) {
-      var jsonResponse = jsonDecode(response.body);
-      return jsonResponse['value'];
-    } else {
-      return ("Unexpected error occurred.");
-    }
+import 'book.dart';
+
+Future<String> getJoke() async {
+  var response = await http.get("https://api.chucknorris.io/jokes/random?category=dev");
+  if (response.statusCode == 200) {
+    var jsonResponse = jsonDecode(response.body);
+    return jsonResponse['value'];
+  } else {
+    return ("Unexpected error occurred.");
   }
+}
+
+Future getBooks() async {
+  const String API_KEY = 'zuwSLbU4yB7m0Qc17dZ5CGzSGT1Z7AuG';
+  var response = await http.get('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=$API_KEY');
+  if (response.statusCode == 200) {
+    var jsonResponse = jsonDecode(response.body);
+    return BookList.fromJson(jsonResponse['results']['books']);
+  } else {
+    print ("Unexpected error occurred.");
+  }
+}
