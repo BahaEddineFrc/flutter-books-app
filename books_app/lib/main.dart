62becca3b8497ddade324ce0044ed5cd88af032a
Baha eddine Ferchichi <baha.ferchichi@aiesec.net>
2021-03-10 15:57:47 +0100

    build book item widget (display image, title, description and author)

--- books_app/lib/main.dart
+++ books_app/lib/main.dart
@@ -51,11 +51,32 @@ class _FirstScreenState extends State<FirstScreen> {
         child: _books != null
             ? ListView.builder(
                 itemCount: _books.results.length,
-                itemBuilder: (context, index) => Text(_books.results[index].title),
+                itemBuilder: (context, index) => _getBookView(_books.results[index]),
               )
             : Center(
                 child: CircularProgressIndicator(),
               ),
+      ),
+    );
+  }
+
+  Widget _getBookView(Book book) {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            child: Image.network(book.bookImage,),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Text('Title : '+book.title),
+                Text('Description : '+book.description),
+                Text('By : '+book.author),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
