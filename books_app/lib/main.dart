b9b7986c37a33c014b28cfe00b66dccd84aedfd5
Baha eddine Ferchichi <baha.ferchichi@aiesec.net>
2021-03-10 11:50:17 +0100

    create FirstScreen with appBar

--- books_app/lib/main.dart
+++ books_app/lib/main.dart
@@ -11,54 +11,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo', //displayed in recent apps (mobile) and tabs titles (web)
       theme: ThemeData(primarySwatch: Colors.red),
-      home: MyHomePage(title: 'Flutter test app'),
+      home: FirstScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar(title: Text('First screen ever')),
+      
     );
   }
 }
