14f41f9db4294894a17105cf33ebb501547c4248
Baha eddine Ferchichi <baha.ferchichi@aiesec.net>
2021-03-10 15:02:53 +0100

    add Book and BookList models

---
+++ books_app/lib/book.dart
@@ -0,0 +1,44 @@
+class Book{
+  int rank;
+  String title;
+  String author;
+  String description;
+  String bookImage;
+
+  Book({
+    this.rank,
+    this.title,
+    this.author,
+    this.description,
+    this.bookImage,
+  });
+
+  /// get model object from Json
+  Book.fromJson(Map<String, dynamic> json) {
+    rank = json['rank'];
+    title = json['title'];
+    author = json['author'];
+    description = json['description'];
+    bookImage = json['book_image'];
+  }
+}
+
+/// a List of books
+class BookList {
+  List<Book> results;
+  List<dynamic> rawData;
+  dynamic error;
+
+  BookList({this.results, this.rawData, this.error});
+
+  /// get List from Json
+  BookList.fromJson(List<dynamic> json) {
+    if (json != null) {
+      results = List<Book>();
+      rawData = json;
+      results = (json).map((i) => Book.fromJson(i)).toList();
+    }
+  }
+}
+
+}
